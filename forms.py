1bbd0dc80b7c760f8b0522c7ca7eba97d5355d93
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-21 09:44:26 -0800

    Created 5 files. Created forms.py, templates/form.html, templates/home.html, templates/login.html, and templates/signup.html.

---
+++ forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired(), Email()
+
+class SignupForm(FlaskForm):
+    """
+        Template for form for users to sign up
+    """
+
+    username = StringField("Username", validators=[InputRequired()])
+
+    email = StringField("Email", validators=[Email()])
+
+    password = PasswordField("Password", validators=[InputRequired()])
+
+    city = StringField("City", validators=[InputRequired()])
+
+    country = StringField("Country", validators=[InputRequired()])
+
+class LoginForm(FlaskForm):
+    """
+        Template for form for users to login
+    """
+
+    username = StringField("Username", validators=[InputRequired()])
+
+    password = PasswordField("Password", validators=[InputRequired()])
