68c09548c78bd7f673cbfa654c710bf1bb9ae653
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-20 16:21:20 -0800

    Added commented out Species model method tests. Added and commented out for now test_get_species(), test_add_species(), and test_delete_species().

--- test_species_model.py
+++ test_species_model.py
@@ -24,15 +24,17 @@ class SpeciesModelTestCase(TestCase):
         City.query.delete()
         Country.query.delete()
 
-        self.species1 = {
+        self.test_species1 = {
             "name": "species1",
             "threatened": "VU"
         }
 
-        self.species2 = {
+        self.test_species2 = {
             "name": "species2",
             "threatened": "VU"
         }
+
+        self.sample_species_name = "loxodonta africana"
         
         self.client = app.test_client()
 
@@ -41,8 +43,8 @@ class SpeciesModelTestCase(TestCase):
             Tests can create species with valid input
         """
 
-        name1 = self.species1["name"]
-        threatened = self.species1["threatened"]
+        name1 = self.test_species1["name"]
+        threatened = self.test_species1["threatened"]
         species1 = Species(name=name1, threatened=threatened)
         db.session.add(species1)
         db.session.commit()
@@ -59,7 +61,7 @@ threatened={threatened}>'
         )
 
         # test can create another species with same threatened level
-        name2 = self.species2["name"]
+        name2 = self.test_species2["name"]
         species2 = Species(name=name2, threatened=threatened)
         db.session.add(species2)
         db.session.commit()
@@ -116,7 +118,7 @@ threatened={threatened}>'
         ) for user in users_list]
         for user in users:
             species1.users.append(user)
-        db.session.commit()
+            db.session.commit()
 
         self.assertEqual(len(species1.users), num_of_users)
         for i in range(num_of_users):
@@ -139,14 +141,14 @@ threatened={threatened}>'
         """
 
         # first create a species to test uniqueness constraints
-        name1 = self.species1["name"]
-        threatened1 = self.species1["threatened"]
+        name1 = self.test_species1["name"]
+        threatened1 = self.test_species1["threatened"]
         species = Species(name=name1, threatened=threatened1)
         db.session.add(species)
         db.session.commit()
 
         # now test creating a new species with invalid inputs
-        threatened2 = self.species2["threatened"]
+        threatened2 = self.test_species2["threatened"]
 
         # test not including name
         species = Species(threatened=threatened2)
@@ -161,3 +163,118 @@ threatened={threatened}>'
         with self.assertRaises(IntegrityError):
             db.session.commit()
         db.session.rollback()
+
+    def test_get_species(self):
+        """
+            Tests can get data on species only if it exists
+        """
+
+        # test if I can get a species that exists but isn't in db
+        name = self.sample_species_name
+        species = Species.get_species(name)
+
+        self.assertIsNotNone(species)
+        self.assertEqual(species.name, name)
+
+        # test if I can get the same species not that it's in db
+        species = Species.get_species(name)
+
+        self.assertIsNotNone(species)
+        self.assertEqual(species.name, name)
+
+        # test that I can't get a species that doesn't exist
+        with self.assertRaises(SpeciesError):
+            species = Species.get_species(name)
+
+    def test_add_species(self):
+        """
+            Tests can add species to user only if not already on user's list
+        """
+
+        # first add a species and a user for testing
+        name = self.test_species1["name"]
+        threatened = self.test_species1["threatened"]
+        species = Species(name=name, threatened=threatened)
+        db.session.add(species)
+        db.session.commit()
+        species_id = species.id
+        
+        country = Country(name="Country")
+        db.session.add(country)
+        db.session.commit()
+        country_id = country.id
+
+        city = City(name="City", country_id=country_id)
+        db.session.add(country)
+        db.session.commit()
+        city_id = city.id
+
+        username = "user"
+        email = "email"
+        password = "password"
+        user = User(
+            username=username,
+            email=email,
+            password=password,
+            city_id=city_id
+        )
+        db.session.add(user)
+        db.session.commit()
+        user_id = user.id
+
+        # test if can add species to user list
+        Species.add_species(species_id, user_id)
+        self.assertEqual(len(user.species), 1)
+        self.assertEqual(user.species[0].name, name)
+        self.assertEqual(user.species[0].threatened, threatened)
+
+        # test that will get error if try to add species already on user's list
+        with self.assertRaises(SpeciesError):
+            Species.add_species(species_id, user_id)
+
+    def test_delete_species(self):
+        """
+            Tests can delete species from user only if in user's list
+        """
+
+        # first add a species to a new user's list
+        name = self.test_species1["name"]
+        threatened = self.test_species1["threatened"]
+        species = Species(name=name, threatened=threatened)
+        db.session.add(species)
+        db.session.commit()
+        species_id = species.id
+        
+        country = Country(name="Country")
+        db.session.add(country)
+        db.session.commit()
+        country_id = country.id
+
+        city = City(name="City", country_id=country_id)
+        db.session.add(country)
+        db.session.commit()
+        city_id = city.id
+
+        username = "user"
+        email = "email"
+        password = "password"
+        user = User(
+            username=username,
+            email=email,
+            password=password,
+            city_id=city_id
+        )
+        db.session.add(user)
+        db.session.commit()
+        user_id = user.id
+
+        user.species.append(species)
+        db.session.commit()
+
+        # test can delete species
+        Species.delete_species(species_id, user_id)
+        self.assertEqual(len(user.species), 0)
+
+        # test that will get error if try to delete species
+        with self.assertRaises(SpeciesError):
+            Species.delete_species(species_id, user_id)
