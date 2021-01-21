96af3a0d0c768aca7491fc630e1c6f30ac0773b3
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-21 10:20:14 -0800

    Fixed typos in and installed email-validator. Fixed typos in forms.py. Updated requirements.txt to reflect new install.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired(), Email()
+from wtforms.validators import InputRequired, Email
 
 class SignupForm(FlaskForm):
     """
