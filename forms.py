4cb99522450f612936f6fd528d61a9f29e49a6fe
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-24 10:55:49 -0800

    Added checkbox for user to agree to terms. Modified SignupForm in forms.py and templates/form.html.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, DataRequired
 
 class SignupForm(FlaskForm):
     """
@@ -16,6 +16,14 @@ class SignupForm(FlaskForm):
     city = StringField("City", validators=[InputRequired()])
 
     country = StringField("Country", validators=[InputRequired()])
+
+    accept_terms = BooleanField(
+        "By checking this box, you understand and agree that if your list of \
+species on the website has a match with a sufficient number of other users, \
+then other users may be notified of your username and email address, and they \
+also then be aware of which city you live in.",
+        validators=[DataRequired()]
+    )
 
 class LoginForm(FlaskForm):
     """
