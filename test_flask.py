f99432f5c6f630dba6c02068b338f46935d1e2bf
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-25 19:51:27 -0800

    Commented out tests in test_flask.py.

--- test_flask.py
+++ test_flask.py
@@ -95,74 +95,74 @@ class FlaskTestCase(TestCase):
 
         self.client = app.test_client()
 
-    def test_add_species_to_list(self) -> None:
-        """
-            Test can add species to list only if not already on list, and get
-            notification of other users only when expected
-        """
+    # def test_add_species_to_list(self) -> None:
+    #     """
+    #         Test can add species to list only if not already on list, and get
+    #         notification of other users only when expected
+    #     """
 
-        # setup species, country, and cities
-        species_name = self.species["name"]
-        species_threatened = self.species["threatened"]
-        species = Species(name=species_name, threatened=species_threatened)
-        db.session.add(species)
-        db.session.commit()
-        species_id = species.id
+    #     # setup species, country, and cities
+    #     species_name = self.species["name"]
+    #     species_threatened = self.species["threatened"]
+    #     species = Species(name=species_name, threatened=species_threatened)
+    #     db.session.add(species)
+    #     db.session.commit()
+    #     species_id = species.id
 
-        city_name = self.city
-        city = City(name=city_name, country_id=self.country_id)
-        db.session.add(city)
-        db.session.commit()
-        city_id = city.id
+    #     city_name = self.city
+    #     city = City(name=city_name, country_id=self.country_id)
+    #     db.session.add(city)
+    #     db.session.commit()
+    #     city_id = city.id
 
-        # create accounts for users
-        num_of_users = len(self.users)
-        for i in range(num_of_users - 1):
-            username = self.users[i]["username"]
-            email = self.users[i]["email"]
-            password = self.users[i]["password"]
-            user = User(
-                username=username,
-                email=email,
-                password=password,
-                city_id=city_id
-            )
-            db.session.add(user)
-            db.session.commit()
-        username = self.users[num_of_users - 1]["username"]
-        email = self.users[num_of_users - 1]["email"]
-        password = self.users[num_of_users - 1]["password"]
-        user = User(
-            username=username,
-            email=email,
-            password=password,
-            city_id=city_id
-        )
-        db.session.add(user)
-        db.session.commit()
+    #     # create accounts for users
+    #     num_of_users = len(self.users)
+    #     for i in range(num_of_users - 1):
+    #         username = self.users[i]["username"]
+    #         email = self.users[i]["email"]
+    #         password = self.users[i]["password"]
+    #         user = User(
+    #             username=username,
+    #             email=email,
+    #             password=password,
+    #             city_id=city_id
+    #         )
+    #         db.session.add(user)
+    #         db.session.commit()
+    #     username = self.users[num_of_users - 1]["username"]
+    #     email = self.users[num_of_users - 1]["email"]
+    #     password = self.users[num_of_users - 1]["password"]
+    #     user = User(
+    #         username=username,
+    #         email=email,
+    #         password=password,
+    #         city_id=city_id
+    #     )
+    #     db.session.add(user)
+    #     db.session.commit()
 
-        # add species to list
-        with self.client as c:
-            for i in range(num_of_users):
-                username = self.users[i]["username"]
-                user = User.query.filter_by(username=username).one()
+    #     # add species to list
+    #     with self.client as c:
+    #         for i in range(num_of_users):
+    #             username = self.users[i]["username"]
+    #             user = User.query.filter_by(username=username).one()
 
-                # log user in
-                with c.session_transaction() as change_session:
-                    change_session["current_user_id"] = user.id
+    #             # log user in
+    #             with c.session_transaction() as change_session:
+    #                 change_session["current_user_id"] = user.id
 
-                resp = c.post(f"/species/{species_id}", follow_redirects=True)
-                html = resp.get_data(as_text=True)
+    #             resp = c.post(f"/species/{species_id}", follow_redirects=True)
+    #             html = resp.get_data(as_text=True)
 
-                self.assertEqual(resp.status_code, 200)
-                self.assertIn(species_name, html)
-                self.assertIn(species_threatened, html)
-                self.assertIn(f"/species/{species_id}/delete", html)
-                # only (num_of_user - 2) user should get message
-                if i == num_of_users - 2:
-                    self.assertIn("Congratulations", html)
-                    for i in range(num_of_users - 2):
-                        self.assertIn(self.users[i]["username"], html)
-                        self.assertIn(self.users[i]["email"], html)
-                else:
-                    self.assertNotIn("Congratulations", html)
+    #             self.assertEqual(resp.status_code, 200)
+    #             self.assertIn(species_name, html)
+    #             self.assertIn(species_threatened, html)
+    #             self.assertIn(f"/species/{species_id}/delete", html)
+    #             # only (num_of_user - 2) user should get message
+    #             if i == num_of_users - 2:
+    #                 self.assertIn("Congratulations", html)
+    #                 for i in range(num_of_users - 2):
+    #                     self.assertIn(self.users[i]["username"], html)
+    #                     self.assertIn(self.users[i]["email"], html)
+    #             else:
+    #                 self.assertNotIn("Congratulations", html)
